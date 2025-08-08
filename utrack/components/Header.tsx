d6b66e118423880c5b395651171dc168a9c3caa0
Irina Yarutkina <irinaarutkina606@gmail.com>
2025-08-08 22:24:15 +0300

    Создан черновой макет главной страницы

---
+++ utrack/components/Header.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from "next/link"; 
+
+export default function Header() {
+  return (
+    <header className="bg-blue shadow-sm sticky top-0 z-50"> 
+        <div className="container mx-auto px-4 py-3 flex items-center justify-between"> 
+            <Link href="/" className="text-xl font-bold text-black-600"> 
+                UTracker | User Tracker 
+            </Link>
+            <nav className="hidden sm:flex gap-6 text-gray-600"> 
+                <Link href="/" className="hover: text-blue-500 transition-colors "> 
+                    Главная
+                </Link>
+            </nav>
+        </div>
+    </header>
+  ); 
+} 
