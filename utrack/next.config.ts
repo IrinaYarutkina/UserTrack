a98371a255e851ae04adfe5053410271ead1e82f
Irina Yarutkina <irinaarutkina606@gmail.com>
2025-08-11 13:03:43 +0300

    правки 2

--- utrack/next.config.ts
+++ utrack/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
+  output: "export",
+
+  webpack(config) {
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
-export default nextConfig;
+export default nextConfig; 
